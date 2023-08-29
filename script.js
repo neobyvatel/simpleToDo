cbac0ffe5ca489bebc2dc745ad76aaf1530f36ca
adilkenzh17 <skeezygod@gmail.com>
2023-08-29 18:41:28 +0600

    refactor: newElement();

--- script.js
+++ script.js
@@ -1,32 +1,20 @@
 const inputBox = document.getElementById("inputBox");
 const listContainer = document.getElementById("todo-list");
-function createListItem(text, checked) {
-  const li = document.createElement("li");
-  const icon = document.createElement("img");
-
-  icon.className = "icon";
-  icon.src = checked ? "images/checked.png" : "images/unchecked.png";
-  li.appendChild(icon);
-  li.appendChild(document.createTextNode(text));
-  if (checked) {
-    li.className = "checked";
-  }
-
-  li.addEventListener("click", function () {
-    toggleChecked(li);
-  });
-  icon.addEventListener("click", function (event) {
-    event.stopPropagation();
-    toggleChecked(li);
-  });
-  listContainer.appendChild(li);
-}
 
 function newElement() {
   if (inputBox.value === "") {
     alert("Invalid input.");
   } else {
-    createListItem(inputBox.value, false);
+    const li = document.createElement("li");
+    const icon = document.createElement("img");
+
+    icon.className = "icon";
+    icon.src = "images/unchecked.png";
+
+    li.append(icon);
+    li.append(inputBox.value);
+
+    listContainer.append(li);
     inputBox.value = "";
   }
 }
@@ -35,9 +23,3 @@ inputBox.addEventListener("keydown", function (event) {
     newElement();
   }
 });
-function toggleChecked(li) {
-  const isChecked = li.classList.contains("checked");
-  const text = li.textContent.trim();
-  li.remove();
-  createListItem(text, !isChecked);
-}
