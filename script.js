b7e7bca6eba66e6fb8eae08c638009323a2a92c9
adilkenzh17 <skeezygod@gmail.com>
2023-08-29 23:31:00 +0600

    refactor all

--- script.js
+++ script.js
@@ -1,25 +1,13 @@
+const list = document.getElementById("todo-list");
 const inputBox = document.getElementById("inputBox");
-const listContainer = document.getElementById("todo-list");
 
 function newElement() {
   if (inputBox.value === "") {
     alert("Invalid input.");
   } else {
-    const li = document.createElement("li");
-    const icon = document.createElement("img");
-
-    icon.className = "icon";
-    icon.src = "images/unchecked.png";
-
-    li.append(icon);
-    li.append(inputBox.value);
-
-    listContainer.append(li);
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    list.appendChild(li);
     inputBox.value = "";
   }
 }
-inputBox.addEventListener("keydown", function (event) {
-  if (event.key === "Enter") {
-    newElement();
-  }
-});
