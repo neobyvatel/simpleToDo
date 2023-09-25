946b1656ce112f9f46cef5069270bc2c05ee60d4
adilkenzh17 <95138445+neobyvatel@users.noreply.github.com>
2023-09-25 14:41:17 +0600

    add data saving(govno)

--- script.js
+++ script.js
@@ -5,19 +5,37 @@ const inputBox = document.getElementById("inputBox");
 const toggleAllButton = document.querySelector(".toggle-all");
 const deleteAllButton = document.querySelector(".remove-all-tasks");
 
+// Load tasks from localStorage on page load
+document.addEventListener("DOMContentLoaded", loadTasks);
+
+function loadTasks() {
+  const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  tasks.forEach((taskText) => {
+    createTaskElement(taskText);
+  });
+}
+
 function newElement() {
   if (inputBox.value === "") {
     showSnackBar();
   } else {
-    let li = document.createElement("li");
-    li.innerHTML = inputBox.value;
-    list.appendChild(li);
-    let span = document.createElement("span");
-    span.innerHTML = "\u00d7";
-    li.append(span);
+    createTaskElement(inputBox.value);
+
+    // Save the task in localStorage
+    saveTask(inputBox.value);
   }
   inputBox.value = "";
 }
+
+function createTaskElement(taskText) {
+  let li = document.createElement("li");
+  li.innerHTML = taskText;
+  list.appendChild(li);
+  let span = document.createElement("span");
+  span.innerHTML = "\u00d7";
+  li.append(span);
+}
+
 inputBox.addEventListener("keypress", (e) => {
   if (e.key === "Enter") {
     newElement();
@@ -31,6 +49,7 @@ list.addEventListener(
       e.target.classList.toggle("checked");
     } else if (e.target.tagName === "SPAN") {
       e.target.parentElement.remove();
+      removeTask(e.target.parentElement.textContent);
     }
   },
   false
@@ -50,10 +69,12 @@ function showSnackBar() {
 }
 
 const listItems = list.children;
+
 function toggleAll() {
   let checked = 0;
   let unchecked = 0;
   let listItem;
+
   for (let i = 0; i < listItems.length; i++) {
     listItem = listItems[i];
     if (listItem.classList.contains("checked")) {
@@ -80,7 +101,26 @@ function removeAll() {
   while (list.firstChild) {
     list.removeChild(list.firstChild);
   }
+
+  // Remove all tasks from localStorage
+  localStorage.removeItem("tasks");
 }
 
 toggleAllButton.addEventListener("click", toggleAll);
 deleteAllButton.addEventListener("click", removeAll);
+
+function saveTask(taskText) {
+  const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  tasks.push(taskText);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function removeTask(taskText) {
+  const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  const trimmedTaskText = taskText.trim();
+  const index = tasks.indexOf(trimmedTaskText);
+  if (index !== -1) {
+    tasks.splice(index, 1);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }
+}
